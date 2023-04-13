6356a6f06407e2bb1cfb776a05937c9399cee215
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-13 15:45:39 +0530

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,14 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        nav: ['Lato'],
+        body: ['Nunito']
+      }
+    }
+  }
 }
