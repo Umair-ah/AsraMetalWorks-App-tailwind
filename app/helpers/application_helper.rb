1d2a8421a2ba866aeb9dc18418774990430cac65
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-14 14:41:42 +0530

    current_page?(path) added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,19 @@
 module ApplicationHelper
     def active?(path)
-        "bg-rose-300" if current_page?(path)
+        if current_page?(path)
+            "bg-rose-300"
+        else
+            "bg-white"
+        end
+
+    end
+
+    def nav?(path)
+        if current_page?(path)
+            "rounded-full text-white bg-rose-500"
+        else
+            ""
+        end
 
     end
 
