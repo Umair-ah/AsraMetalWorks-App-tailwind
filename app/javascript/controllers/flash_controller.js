8287128cb9db3a6797d8bf99f070b2e110f12276
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-14 17:53:36 +0530

    styled forms, navs

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    
+  connect() {
+    setTimeout(() => {
+        this.dismiss();
+    }, 6400);
+  }
+
+  dismiss() {
+    this.element.remove();
+  }
+}
